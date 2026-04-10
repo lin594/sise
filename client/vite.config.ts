6c23aca0cb149ea825f157a722ebab91772fcd44
lin594 <lin.yan@bupt.edu.cn>
2026-04-10 09:59:45 +0800

    build: improve docker-compose.yml

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".mjs", ".json", ".vue"],
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".mjs", ".json", ".vue"],
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
+      allowedHosts: true,
+    },
+  };
 });
